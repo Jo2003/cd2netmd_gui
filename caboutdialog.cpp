c7bd798c50e1bbb0afb1e79dddfc90eb0282e719
Jo2003 <olenka.joerg@gmail.com>
2022-01-05 15:16:13 +0100

    better style for about dialog

--- caboutdialog.cpp
+++ caboutdialog.cpp
@@ -26,16 +26,16 @@ CAboutDialog::CAboutDialog(QWidget *parent) :
     ui->labelName->setText(QString("%1 %2 %3").arg(c2n::PROGRAM_NAME).arg(c2n::PROGRAM_VERSION).arg(__DATE__));
     QString text =
 R"(<b>CD to NetMD GUI - a tool like Sonys NetMD Simply Burner</b><br>
-Project site: <a href="https://github.com/Jo2003/cd2netmd_gui">github.com</a><br>
+Project site: <a style="color: #3499b3; font-weight: bold;" href="https://github.com/Jo2003/cd2netmd_gui">github.com</a><br>
 We are using following projects:<br>
 <ul>
-    <li><a href="https://www.gnu.org/software/libcdio/">libcdio</a> with CDDA paranoia extension.</li>
-    <li><a href="https://github.com/nlohmann/json">json.hpp - JSON for modern C++</a></li>
-    <li><a href="https://github.com/dcherednik/atracdenc">atracdenc - ATRAC Decoder Encoder</a></li>
-    <li><a href="https://github.com/Jo2003/netmd">linux-minidisc</a></li>
-    <li><a href="https://www.qt.io/">Qt - One framework to rule them all</a></li>
+    <li><a style="color: #3499b3; font-weight: bold;" href="https://www.gnu.org/software/libcdio/">libcdio</a> with CDDA paranoia extension.</li>
+    <li><a style="color: #3499b3; font-weight: bold;" href="https://github.com/nlohmann/json">json.hpp - JSON for modern C++</a></li>
+    <li><a style="color: #3499b3; font-weight: bold;" href="https://github.com/dcherednik/atracdenc">atracdenc - ATRAC Decoder Encoder</a></li>
+    <li><a style="color: #3499b3; font-weight: bold;" href="https://github.com/Jo2003/netmd">linux-minidisc</a></li>
+    <li><a style="color: #3499b3; font-weight: bold;" href="https://www.qt.io/">Qt - One framework to rule them all</a></li>
 </ul>
-Please support me through <a href="https://paypal.me/Jo2003">Paypal</a>!)";
+Please support me through <a style="color: #3499b3; font-weight: bold;" href="https://paypal.me/Jo2003">Paypal</a>!)";
     ui->textAbout->setHtml(text);
 }
 
