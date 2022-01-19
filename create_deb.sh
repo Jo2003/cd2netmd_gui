0812001041bd581910205ae565a913f6b89be63e
Jo2003 <olenka.joerg@gmail.com>
2022-01-19 12:33:16 +0100

    prepare first linux build

--- create_deb.sh
+++ create_deb.sh
@@ -63,7 +63,7 @@ Installed-Size: $(($(du -b --max-depth=0 ${BUILD_FOLDER}/usr|gawk '{print $1}') 
 EOF
 
     cat << EOF >> "${BUILD_FOLDER}/DEBIAN/control"
-Depends: gtk2-engines-pixbuf (>= 2.24.10), libqt5help5 (>= 5.0.2), libqt5network5 (>= 5.0.2), libqt5sql5-sqlite (>= 5.0.2), libqt5xml5 (>= 5.0.2), libqt5core5a (>= 5.0.2), libqt5gui5 (>= 5.0.2), libvlc5 (>= 2.0.8), vlc (>= 2.0.8), libc6 (>= 2.15)
+Depends: libsndfile1 (>= 1.0.28), libflac8 (>= 1.3.2), libogg0 (>= 1.3.2), libvorbis0a (>= 1.3.5), libvorbisenc2 (>= 1.3.5), libcdio17 (>= 1.0.0), libcdio-cdda2 (>= 10.2), libcdio-paranoia2 (>= 10.2), libgcrypt20 (>= 1.8.1), libusb-1.0-0 (>= 2:1.0.21), libjson-c3 (>= 0.12.1), libqt5widgets5 (>= 5.9.5), libqt5gui5 (>= 5.9.5), libqt5network5 (>= 5.9.5), libqt5core5a (>= 5.9.5), libgpg-error0 (>= 1.27), libssl1.1 (>= 1.1.1)
 EOF
 fi
 
