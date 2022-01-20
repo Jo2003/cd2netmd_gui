a895a103ab63832439de87e5c5d2d28da06ff8a7
Jo2003 <olenka.joerg@gmail.com>
2022-01-20 15:57:14 +0100

    take care for changed exe name

--- create_pack.sh
+++ create_pack.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf install
 mkdir install
-cp release/QCD2NetMD.exe install/
+cp release/cd2netmd_gui.exe install/QCD2NetMD.exe
 windeployqt install/QCD2NetMD.exe
 deps=$(ldd install/QCD2NetMD.exe | gawk '/mingw64/ {print $3}'); for d in $deps ; do cp -v $d install/ ; done
 cp -rv toolchain install/
